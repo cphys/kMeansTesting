68d39450f7f3c505e51f4d6f86da0aa8d310f525
cphys <22038435+cphys@users.noreply.github.com>
2019-06-16 18:04:15 -0400

    created function to plot k-means data from a data frame

--- example.py
+++ example.py
@@ -1,6 +1,8 @@
 import numpy as np
+import pandas as pd
 from sklearn.datasets.samples_generator import make_blobs 
 from kMeansFun import kMeansPlot
+from kMeansFun import kMeansPlotClust
 
 '''
 create random clusters of points using make_blobs class in sklearn package
@@ -27,4 +29,26 @@ stdBlobs = 0.9
 
 coords, labs = make_blobs(n_samples = numSamp, centers = clCents, cluster_std = stdBlobs)
 
+randCoords = {'x values':coords[:,0],'y values':coords[:,1]}
+dfRand = pd.DataFrame.from_dict(randCoords)
+kMeansPlotClust(dfRand,'x values','y values',saveFile=True,pltTitle='Random Clusters', numClust = 4)
+
+
+
+'''
+Dataset downloaded from IBM storage as part of customer segmentation lab. Goal
+is to partition customer base into groups based on set of collected data.
+'''
+## Reading in the data and creating a dataframe using pandas ##
+# dfCust = pd.read_csv("https://s3-api.us-geo.objectstorage.softlayer.net/cf-courses-data/CognitiveClass/ML0101ENv3/labs/Cust_Segmentation.csv")
+
+## Save the data set as a csv ##
+# df.to_csv("cust_Segmentation.csv", index=False)
+
+dfCust = pd.read_csv("cust_Segmentation.csv")
+kMeansPlotClust(dfCust,'Age','Income',size='Edu', dropValues=['Address','Customer Id'],saveFile=True,pltTitle='Customer Segmentation',nInit=1000,scType=False,showCent=False)
+
+
+'''
 kMeansPlot(coords, labs, numClust = 4, tempColorFix=False,saveFile = True)
+'''
--- kMeansFun.py
+++ kMeansFun.py
@@ -1,7 +1,11 @@
 import matplotlib.pyplot as plt
 import numpy as np
+import pandas as pd
 import os
-from sklearn.cluster import KMeans 
+from sklearn.cluster import KMeans
+
+# from sklearn.preprocessing import StandardScaler
+# from sklearn.preprocessing import MinMaxScaler 
 
 
 
@@ -38,7 +42,7 @@ def kMeansPlot(data, labels, numClust=False, kMehtod = "k-means++", nInit = 12, 
     if not numClust:
         numClust = dataClust
     
-    kMeansCl = KMeans(init = "k-means++", n_clusters = numClust, n_init = nInit)
+    kMeansCl = KMeans(init = kMehtod, n_clusters = numClust, n_init = nInit)
 
     kMeansCl.fit(data) # Fit k-means model to data
 
@@ -91,29 +95,6 @@ def kMeansPlot(data, labels, numClust=False, kMehtod = "k-means++", nInit = 12, 
         # Plots the centroids with specified color, but with a darker outline
         ax2.scatter(kMeansCentroid[0], kMeansCentroid[1], alpha = 1, c = [color], edgecolors = 'k', s=30)
 
-    '''
-    for k, color in zip(range(numClust), colors):
-
-        # Create a list of all data points, where the data poitns that are 
-        # in the cluster (ex. cluster 0) are labeled as true, else they are
-        # labeled as false.
-        clustMemb1 = (labels == k)
-        clustMemb = (kMeansLabs == k)
-        
-        # Define the centroid, or cluster center.
-        kMeansCentroid = kMeansClCents[k]
-        print(len(colors))
-        if tempColorFix:
-            color2 = [colors[i + (-1)**i] for i in range(len(colors))][k]
-        else:
-            color2 = color
-        # Plots the datapoints with color.
-        ax1.scatter(data[clustMemb1, 0], data[clustMemb1, 1],alpha = 0.33, c = [color2], edgecolors = [color2], s=5)
-        ax2.scatter(data[clustMemb, 0], data[clustMemb, 1],alpha = 0.33, c = [color], edgecolors = [color], s=5)
-        
-        # Plots the centroids with specified color, but with a darker outline
-        ax2.scatter(kMeansCentroid[0], kMeansCentroid[1],alpha = 1, c = [color], edgecolors = 'k', s=30)
-    '''
     # Title of the plot
     ax1.set_title('randomly generated data')
     ax2.set_title('k-means data')
@@ -123,3 +104,130 @@ def kMeansPlot(data, labels, numClust=False, kMehtod = "k-means++", nInit = 12, 
     if saveFile:
         plt.savefig(os.path.join(dirName, 'kMeansClust.png'), bbox_inches='tight')
 
+
+def kMeansPlotClust(df,xAxis,yAxis,size=False,sScale=1,dropValues=False, numClust = 3, scType = 'StandardScaler', kMehtod = "k-means++", nInit = 12, saveFile = False, dirName = os.getcwd(), scale = 5, ratio = .75, tempColorFix = True, fontSize = 18, pltTitle=False,showCent=True):
+
+    """
+    Function which uses sklearn machine learning package to perform a
+    k-means test. Function will group clustered data and output a plot showing
+    the k-means test data and centroids
+
+    Parameters:
+    -----------
+    df :           a pandas dataframe
+    xAxis:         dataframe column header to be plotted on the x axis.
+    yAxis:         dataframe column header to be plotted on the y axis.
+    size:          dataframe column header associated with the size of the dots.
+    sScale:        scale for dots in the scatter plot.
+    dropValues:    values to be dropped from the dataframe before the kmeans
+                   test. Used with non-numerical values
+    numClust:      number of clusters in the k-means test. If false default is 
+                   the number of unique values in labels.
+    scType:        scaling techniques associated with sci-kit learn package.
+    kMehtod:       method used to initialize the centroids
+    nInit:         number of times to run k-means algorithm with different
+                   centroid seeds.
+    dirName :      (str) name of the directory to which the file should be saved
+    tempColorFix:  Changes the ordering of the colors in the original data to
+                   match the k-means test data. This is a temporary fix for the
+                   existing data and needs to be changed to handle different 
+                   data sets.
+
+    Returns:
+    --------
+    A plot of scaled data separated by k-means labels. Includes
+    centroids.
+    """
+
+    # here we want to clean the dataset. Because Address, in this form, is not
+    # meaningful in a Euclidean space we drop the function.
+    if dropValues != False:
+        df = df.drop(dropValues, axis=1)
+
+    # Before defining a Eclidean distance we must first normalize the data
+    # This can be done easily with sklearn function StandardScaler
+
+    # create numpy arrays for each row, excluding the first column.
+    dfValues = df.values
+
+    # replace nan with 0 and inf(-inf) with large(small) float.
+    # in general just replacing nan with 0 is not the appropriate way to 
+    # go forward, but this was done in example, should fix later.
+    dfValues = np.nan_to_num(dfValues) 
+
+
+    if scType == False:
+        normData = dfValues
+    else:
+        if scType == 'StandardScaler':
+            from sklearn.preprocessing import StandardScaler
+            scaler = StandardScaler()
+        elif scType == 'MinMaxScaler':
+            from sklearn.preprocessing import MinMaxScaler
+            scaler = MinMaxScaler()
+        elif scType == 'minmax_scale':
+            from sklearn.preprocessing import minmax_scale
+            scaler = minmax_scale()
+        elif scType == 'MaxAbsScaler':
+            from sklearn.preprocessing import MaxAbsScaler
+            scaler = MaxAbsScaler()
+        elif scType == 'RobustScaler':
+            from sklearn.preprocessing import RobustScaler
+            scaler = RobustScaler()
+        elif scType == 'Normalizer':
+            from sklearn.preprocessing import Normalizer
+            scaler = Normalizer()
+        elif scType == 'QuantileTransformer':
+            from sklearn.preprocessing import QuantileTransformer
+            scaler = QuantileTransformer()
+        elif scType == 'PowerTransformer':
+            from sklearn.preprocessing import PowerTransformer
+            scaler = PowerTransformer()
+        normData = scaler.fit_transform(dfValues)     
+
+    dfNorm = pd.DataFrame(normData, columns=df.columns, index=df.index)
+
+    # Apply k-means to the dataset and look at the labels
+    kMeansDat = KMeans(init = kMehtod, n_clusters = numClust, n_init = nInit)
+
+    kMeansDat.fit(normData)
+
+    # gives the label of the cluster data based on the k-means model #
+    kMeansLabs = kMeansDat.labels_
+
+    # gives the center of the cluster data based on the k-means model #
+    kMeansClCents = kMeansDat.cluster_centers_
+
+    # Assign labels to each row in the dataframe
+    dfNorm['k means clusters'] = kMeansLabs
+
+    # Print the centroid values
+    # print(df.groupby('k means clusters').mean())
+
+    # Initialize the plot with the specified dimensions.
+    fig = plt.figure(figsize=(scale, ratio * scale))
+    # Create plots
+    ax1 = fig.add_subplot(111)
+
+    # plot the distribution of customers based on their age and income
+    # the size of the circles represents education level
+    if not size:
+        area = sScale * 30
+    else:
+        area = sScale * np.pi * (df[size])**2
+    normLabs = kMeansLabs.astype(np.float)/np.max(kMeansLabs)
+    colors = plt.cm.Spectral(normLabs)
+    ax1.scatter(dfNorm[xAxis], dfNorm[yAxis], s=area, c= colors, alpha=0.45, edgecolors = 'k')
+
+    # Plots the datapoints with color.
+    if showCent:
+        ax1.scatter(kMeansClCents[:,0],kMeansClCents[:,1], alpha = 1, c = 'b', edgecolors = 'r', s = sScale * 45)
+
+    ax1.set_xlabel(xAxis.title().replace("_"," "),fontsize=fontSize)
+    ax1.set_ylabel(yAxis.title().replace("_"," "),fontsize=fontSize)
+
+    if pltTitle != False:
+        ax1.set_title(pltTitle.title().replace("_"," "),fontsize=fontSize)
+
+    if saveFile:
+        plt.savefig(os.path.join(dirName,'{}_{}_vs_{}'.format(pltTitle.replace(" ","_"),xAxis.replace(" ","_"),yAxis.replace(" ","_"))), bbox_inches='tight')
